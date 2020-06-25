6e7a797e448ed7824b8cf2a8767e86b9be990ccd
bellomusodiq <bmayowa25@gmail.com>
2020-06-25 18:00:43 +0100

    Updates

---
+++ precache-manifest.d42268c1b28108a2e9c60538dbfb3821.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4edbc91a67e6c27124d67302e497a81a",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "e3030029a3020ea669eb",
+    "url": "/portfolio/static/css/main.fcc831b2.chunk.css"
+  },
+  {
+    "revision": "bf0ce79c2f2fc4e6dcef",
+    "url": "/portfolio/static/js/2.44109d98.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/portfolio/static/js/2.44109d98.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3030029a3020ea669eb",
+    "url": "/portfolio/static/js/main.e67e2834.chunk.js"
+  },
+  {
+    "revision": "dd58a8ce79d9ed6cb63b",
+    "url": "/portfolio/static/js/runtime-main.80ed0b8a.js"
+  },
+  {
+    "revision": "bdc01c7d62b7f78f88aca446b1a4242a",
+    "url": "/portfolio/static/media/Wallpapper.bdc01c7d.png"
+  },
+  {
+    "revision": "ae0b9b524b5beb93cb129f1703b8a9d7",
+    "url": "/portfolio/static/media/logo.ae0b9b52.png"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/portfolio/static/media/logo.ee7cd8ed.svg"
+  }
+]);
